9fbed6c224eb2ec622ccee94ba3d4cdd0bcbc6d5
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2024-03-10 20:05:57 +0100

    Add project files.

---
+++ Uppgift 6.12/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Uppgift_6._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bestäm höjden:");
+            int höjd = int.Parse(Console.ReadLine());
+            Triangel(höjd);
+            Console.ReadKey();
+        }
+
+        static void Triangel(int höjd)
+        {
+            for(int i = 0; i < höjd; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("*");
+                }
+                else
+                {
+                    for (int j = 0; j < i + 2; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
